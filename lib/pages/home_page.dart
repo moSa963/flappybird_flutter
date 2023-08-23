b0f1e5203b5974955cde4fea8fa6c78165ca677f
M S <mo.sa963@hotmail.com>
2023-08-23 22:09:30 +0300

    Add a temporary home page implementation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,7 @@
+import 'package:flappybird_flutter/models/box_model.dart';
+import 'package:flappybird_flutter/widgets/background.dart';
+import 'package:flappybird_flutter/widgets/bird.dart';
+import 'package:flappybird_flutter/widgets/pipes_sheet.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,12 +12,55 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  BoxModel? box;
+  BoxModel? gap;
+  bool active = false;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Placeholder(),
-    );
-  }
+    return Scaffold(
+        body: Container(
+      color: Colors.transparent,
+      constraints: const BoxConstraints(minHeight: 1000),
+      clipBehavior: Clip.none,
+      child: Stack(
+        children: [
+          Background(),
+          PipesSheet(
+            active: active,
+            onChange: (box, pipe) {
+              this.box = box;
+              gap = pipe;
+            },
+          ),
+          Bird(
+            onTap: () {
+              if (active) return;
+              setState(() {
+                active = true;
+              });
+            },
+            active: active,
+            onMoved: (birdBox) {
+              if (birdBox!.bottom > box!.bottom || birdBox!.top < box!.top) {
+                setState(() {
+                  active = false;
+                });
+              }
 
+              if (gap == null) return;
+
+              if (birdBox.right >= gap!.left && birdBox.left < gap!.right) {
+                if (birdBox.top < gap!.top || birdBox.bottom > gap!.bottom) {
+                  setState(() {
+                    active = false;
+                  });
+                }
+              }
+            },
+          ),
+        ],
+      ),
+    ));
+  }
 }
