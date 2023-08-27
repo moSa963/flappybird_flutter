a61897f96e74312f3025ec28263de57ab6372071
M S <mo.sa963@hotmail.com>
2023-08-27 18:17:20 +0300

    Add timer and _reset method to PipesSheet

--- lib/widgets/pipes_sheet.dart
+++ lib/widgets/pipes_sheet.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flappybird_flutter/models/box_model.dart';
 import 'package:flappybird_flutter/models/pipe_model.dart';
 import 'package:flappybird_flutter/widgets/ground.dart';
@@ -17,10 +19,10 @@ class _PipesSheetState extends State<PipesSheet> {
   final _boxkey = GlobalKey();
   BoxModel? _gapBox;
 
-  List<PipeModel> pipes = [];
+  List<PipeModel> _pipes = [];
   double _gap = 0;
   double _groundOffset = 0;
-
+  Timer? _timer;
 
   @override
   void initState() {
@@ -30,8 +32,11 @@ class _PipesSheetState extends State<PipesSheet> {
   @override
   void didUpdateWidget(covariant PipesSheet oldWidget) {
     super.didUpdateWidget(oldWidget);
-    if (oldWidget.active) return;
-    _start();
+    if (!oldWidget.active) {
+      _start();
+    } else {
+      _reset();
+    }
   }
 
   @override
@@ -43,8 +48,12 @@ class _PipesSheetState extends State<PipesSheet> {
           key: _boxkey,
           child: Stack(
             children: [
-              for (var i = 0; i < pipes.length; ++i)
-                Pipe(key: Key("pipe ${pipes[i].key}"), offset: pipes[i].offset, onChange: i == 0 ? (p0) => (_gapBox = p0) : null,)
+              for (var i = 0; i < _pipes.length; ++i)
+                Pipe(
+                  key: Key("pipe ${_pipes[i].key}"),
+                  offset: _pipes[i].offset,
+                  onChange: i == 0 ? (p0) => (_gapBox = p0) : null,
+                )
             ],
           ),
         ),
@@ -53,8 +62,10 @@ class _PipesSheetState extends State<PipesSheet> {
             if (-_groundOffset >= constraints.biggest.width) {
               _groundOffset = 0;
             }
-            
-            return Ground(dx: _groundOffset,);
+
+            return Ground(
+              dx: _groundOffset,
+            );
           },
         )
       ],
@@ -62,23 +73,29 @@ class _PipesSheetState extends State<PipesSheet> {
   }
 
   void _addNewPipe() {
-    final key = (pipes.lastOrNull?.key ?? 0);
-    pipes.add(PipeModel(key: key + 1, offset: MediaQuery.of(context).size.width));
+    final key = (_pipes.lastOrNull?.key ?? 0);
+    _pipes.add(
+        PipeModel(key: key + 1, offset: MediaQuery.of(context).size.width));
   }
 
   void _start() async {
-    while (widget.active) {
+    if (_timer?.isActive ?? false) return;
+    DateTime last = DateTime.now();
+
+    _timer = Timer.periodic(const Duration(milliseconds: 5), (timer) {
+      int dt = DateTime.now().difference(last).inMilliseconds;
+
       setState(() {
-        for (int i = 0; i < pipes.length; ++i) {
-          if (pipes[i].offset <= -Pipe.width) {
-            pipes.removeAt(0);
+        for (int i = 0; i < _pipes.length; ++i) {
+          if (_pipes[i].offset <= -Pipe.width) {
+            _pipes.removeAt(0);
             --i;
             continue;
           }
 
-          pipes[i].offset -= 0.4;
+          _pipes[i].offset -= 0.16 * dt;
         }
-        _groundOffset -= 0.4;
+        _groundOffset -= 0.16 * dt;
 
         if (_gap >= 1000) {
           _addNewPipe();
@@ -86,11 +103,24 @@ class _PipesSheetState extends State<PipesSheet> {
         }
       });
 
-      ++_gap;
+      _gap += dt * .4;
 
-      widget.onChange?.call(BoxModel.fromRenderBox(_boxkey.currentContext?.findRenderObject() as RenderBox), _gapBox);
+      last = DateTime.now();
 
-      await Future.delayed(const Duration(milliseconds: 1));
-    }
+      widget.onChange?.call(
+          BoxModel.fromRenderBox(
+              _boxkey.currentContext?.findRenderObject() as RenderBox),
+          _gapBox);
+    });
+  }
+
+  void _reset() {
+    setState(() {
+      _timer?.cancel();
+      _gapBox = null;
+      _pipes = [];
+      _gap = 0;
+      _groundOffset = 0;
+    });
   }
 }
